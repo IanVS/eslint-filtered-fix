ef31a8d367c624febdedfc62f724b1355568d708
RedMser <5117197+RedMser@users.noreply.github.com>
2022-01-16 17:14:43 +0100

    Allow specifying array argument multiple times
    This is already mentioned in the readme, but the "concatRepeatedArrays" flag of optionator is set to false by default, so this feature did not work.

--- src/options.js
+++ src/options.js
@@ -30,11 +30,13 @@ module.exports = optionator({
     type       : '[String]',
     default    : '.js',
     description: 'Specify JavaScript file extensions',
+    concatRepeatedArrays: true,
   }, {
     option     : 'rule',
     alias      : 'r',
     type       : '[String]',
     description: 'Specify one or more rules to fix',
+    concatRepeatedArrays: true,
   }, {
     option     : 'warnings',
     alias      : 'w',
